38afd15214f67103d3de3cbb7ee8b8a8a7b41eeb
yeeysonduarte <portas3@hotmail.com>
2023-11-24 10:11:33 +0000

    change images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,13 +10,13 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
+      <img src="https://images.pexels.com/photos/18925469/pexels-photo-18925469/free-photo-of-praia-litoral-frio-com-frio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://images.pexels.com/photos/19035601/pexels-photo-19035601/free-photo-of-pano-de-fundo-contexto-praia-litoral.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
       <img src="https://images.pexels.com/photos/18714268/pexels-photo-18714268/free-photo-of-frio-com-frio-gelado-congelado.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.pexels.com/photos/14541415/pexels-photo-14541415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.pexels.com/photos/5804450/pexels-photo-5804450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
