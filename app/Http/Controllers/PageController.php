55b265083105bc1a580ac7681679fa629ccba0ea
yeeysonduarte <portas3@hotmail.com>
2023-12-11 20:08:55 +0000

    add create page form, controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePageRequest;
 use App\Http\Requests\UpdatePageRequest;
 use App\Models\Page;
+use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -20,6 +21,9 @@ class PageController extends Controller
     public function index()
     {
         $pages = Page::all();
+        //var_dump($pages);
+        //dd($pages);
+        dump($pages); // love dump
         return view('admin.pages.pages.index', ['pages' => $pages]);
     }
 
@@ -28,15 +32,39 @@ class PageController extends Controller
      */
     public function create()
     {
-        //
+        $pages = Page::all();
+        return view('admin.pages.pages.create', ['pages' => $pages]);
     }
 
     /**
      * Store a newly created resource in storage.
+
      */
-    public function store(StorePageRequest $request)
+    public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+           // 'is_active' => 'boolean',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust as needed
+        ]);
+
+        $page = new Page();
+        $page->title = $request->title;
+        $page->description = $request->description;
+        $page->image = $request->image;
+        $page->is_active = $request->has('is_active') ? true : false;
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $imageName = $page->title . '_' . date('Y-m-d') . '_' . $image->getClientOriginalName();
+            $path = $request->file('image')->storeAs('images/pages/' . $page->id, $imageName, 'public');
+            //$image->move(storage_path('app/public'), $imageName);
+            $page->image = $path;
+        }
+        $page->save();
+
+        return redirect('admin/page')->with('success', 'Page created successfully.');
     }
 
     /**
