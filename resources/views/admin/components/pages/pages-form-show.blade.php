590a0350a79ad4c7a7e89e6ea8de281b3bbf5e8c
yeeysonduarte <portas3@hotmail.com>
2024-02-08 17:44:59 +0000

    layout fixes

--- resources/views/admin/components/pages/pages-form-show.blade.php
+++ resources/views/admin/components/pages/pages-form-show.blade.php
@@ -1,34 +1,34 @@
 <div class="container">
-    <h2>Show - {{$page->title}}</h2>
+    <h2>Show</h2>
     <a href="{{ url('admin/page') }}" class="btn btn-primary">Back</a>
 
-    <h3> {{$page->title}}</h3>
+    <h3 class="mt-4"> {{$page->title}}</h3>
+    <div class="row mb-4">
+        <div class="col">
+            <strong>Image</strong>
+            <div class="w-50">
+                <!----->
+                @if ($page->image)
+                <img class="w-50 img-thumbnail" src="{{ asset('storage/' . $page->image) }}" alt="image"></td>
+                @else
+                <p>No Image</p>
+                @endif
+            </div>
+        </div>
+        <div class="col">
+            <div>
+                <strong>Status</strong>
+                @if($page->is_active === 1)
+                <p>Active</p>
+                @else
+                <p>Inactive</p>
+                @endif
+            </div>
+        </div>
+    </div>
 
     <strong>Description</strong>
     <div class="show-style">
         {!!$page->description!!}
     </div>
-
-    <strong>Image</strong>
-    <div class="w-25 ">
-        <!----->
-        @if ($page->image)
-        <img class="w-50 img-thumbnail" src="{{ asset('storage/' . $page->image) }}" alt="image"></td>
-        @else
-        <p>No Image</p>
-        @endif
-    </div>
-    <!----->
-    <div>
-        <span class="invalid-feedback" role="alert"></span>
-    </div>
-    <div>
-        @if($page->is_active === 1)
-        <p>Active</p>
-        @else
-        <p>Inactive</p>
-        @endif
-
-    </div>
-    </form>
 </div>
--- resources/views/admin/master/footer.blade.php
+++ resources/views/admin/master/footer.blade.php
@@ -1,5 +1,5 @@
-   <!-- Copyright -->
-   <div class="text-center p-2 text-bg-dark" >
-        <p class=" mb-0"> &copy; {{ now()->format('Y') }} Design by Yeeyson</p>
-    </div>
-    <!-- Copyright -->
+<!-- Copyright -->
+<div class="text-center p-2 text-bg-dark">
+    <p class=" mb-0"> &copy; {{ now()->format('Y') }} Design by Yeeyson</p>
+</div>
+<!-- Copyright -->
