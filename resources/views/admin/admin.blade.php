b60e3f8b0c4f1f6f48b15f85d12f09b75e8c2ef0
yeeysonduarte <portas3@hotmail.com>
2024-02-09 12:19:59 +0000

    fix admin blade

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -7,7 +7,7 @@
               <div class="col-md-8">
                   <div class="card">
                       <div class="card-header">{{ __('Dashboard') }}</div>
-      
+
                       <div class="card-body">
                           @if (session('status'))
                               <div class="alert alert-success" role="alert">
@@ -21,15 +21,7 @@
               </div>
           </div>
         </div>
-     <div class="row">
-        <div class="col">
 
-            <div> <!--- Sliders Component--->  
-                @component('admin.components.sliders.sliders-list',['sliders' => $sliders] )
-                @endcomponent
-            </div>
-        </div>
-     </div>
 
 </div>
 @endsection
