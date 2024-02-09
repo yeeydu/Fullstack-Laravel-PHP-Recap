51b461788b816762634bde65fd8c01f556b97868
yeeysonduarte <portas3@hotmail.com>
2024-02-09 12:19:35 +0000

    fix dashboard link

--- resources/views/admin/master/sidebar.blade.php
+++ resources/views/admin/master/sidebar.blade.php
@@ -12,18 +12,10 @@
                         </a>
                     </li>
                     <li class="@if(Request::path()== 'admin/dashboard') active @endif">
-                        <a href="{{url('admin/dashboard')}}" data-bs-toggle="collapse"
+                        <a href="{{ url('admin/dashboard') }}"
                             class="nav-link px-0 align-middle">
                             <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
                         </a>
-                        {{-- <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                            </li>
-                        </ul> --}}
                     </li>
                     <li class="@if(Request::path()== 'admin/page') active @endif">
                         <a href="{{url('admin/page')}}" class="nav-link px-0 align-middle">
