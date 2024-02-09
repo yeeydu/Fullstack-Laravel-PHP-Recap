31fe427953eb01f8e24d3e69d253f4177576078b
yeeysonduarte <portas3@hotmail.com>
2024-02-09 16:24:23 +0000

    add update method to sliders

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreSliderRequest;
 use App\Http\Requests\UpdateSliderRequest;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SliderController extends Controller
 {
@@ -48,7 +49,6 @@ class SliderController extends Controller
         $slider->order = $request->order;
         $slider->save();
 
-            dump($request->image);
         if($request->hasFile('image')){
             $imagePath = $request->file('image');
             $imageName = $slider->id . '_' . $slider->title . '_' . date('Y-m-d') . '_' . $imagePath->getClientOriginalName();
@@ -67,7 +67,7 @@ class SliderController extends Controller
      */
     public function show(Slider $slider)
     {
-       
+
         return view('admin.pages.sliders.show', ['slider' => $slider]);
     }
 
@@ -76,15 +76,43 @@ class SliderController extends Controller
      */
     public function edit(Slider $slider)
     {
-        //
+        return view('admin.pages.sliders.edit', ['slider' => $slider]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateSliderRequest $request, Slider $slider)
+    public function update(Request $request, Slider $slider)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'link' => 'required|string',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+           // 'is_active' => 'boolean',
+            'order' => 'required',
+        ]);
+
+        $slider = Slider::find($slider->id);
+        $slider->title = $request->title;
+        $slider->description = $request->description;
+        $slider->link = $request->link;
+        $slider->is_active = $request->has('is_active') ? 1 : 0;
+        $slider->order = $request->order;
+        $slider->save();
+
+        if($request->hasFile('image')){
+            Storage::deleteDirectory('public/images/slider/' . $slider->id);
+
+            $imagePath = $request->file('image');
+            $imageName = $slider->id . '_' . $slider->title . '_' . date('Y-m-d') . '_' . $imagePath->getClientOriginalName();
+            $path = $request->file('image')->storeAs('images/sliders/'. $slider->id, $imageName, 'public');
+            $slider->image = $path;
+            $slider->save();
+        }
+
+        $slider->image = $request->image;
+        return redirect('admin/dashboard')->with('msg', 'Item created successfully');
     }
 
     /**
--- resources/views/admin/components/sliders/slider-form-edit.blade.php
+++ resources/views/admin/components/sliders/slider-form-edit.blade.php
@@ -1,9 +1,8 @@
-
 <div id="media" class="container">
     <h2>Edit fotografia</h2>
-    <a href="{{ url('admin/') }}" class="btn btn-primary">Back</a>
+    <a href="{{ url('admin/dashboard') }}" class="btn btn-primary">Back</a>
     <form method="POST" action="{{url('admin/sliders/'. $slider->id)}}" enctype="multipart/form-data">
-         @csrf
+        @csrf
         @method('PUT')
         <div class="form-group">
             <label for="exampleInputPassword1">Titulo</label>
@@ -16,37 +15,71 @@
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Descrição</label>
-            <textarea rows="6" type="text"  name="description" id="description" autocomplete="description" placeholder="Type your description"
-            class="editor form-control @error('description')
+            <textarea rows="6" type="text" name="description" id="description" autocomplete="description"
+                placeholder="Type your description" class="editor form-control @error('description')
                     is-invalid
-                @enderror"
-            value="{{ $slider->description }}"  aria-describedby="nameHelp">{{ $slider->description }}</textarea>
-            @error('description')  <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-            @enderror
-        </div>
-             <div class="form-group"> <!-----image----->
-                <label for="exampleInputPassword1">Imagem</label>
-                <!---- filenames[]----->
-                <input type="file"  name="image" id="image"
-                class="form-control @error('name')
-                        is-invalid
-                    @enderror"
-                value="{{ $slider->image }}"  aria-describedby="nameHelp">
-                @error('image')  <span class="invalid-feedback" role="alert">
+                @enderror" value="{{ $slider->description }}"
+                aria-describedby="nameHelp">{{ $slider->description }}</textarea>
+            @error('description') <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
                 @enderror
-        </div> <!----------->
-        <div class="w-50 "> <!-- show player image--->
-             @if ($slider->image)
-                <img class=" w-25  img-thumbnail" src="{{ asset('storage/' . $slider->image) }}" alt="image"></td>
-             @else
-                <p>No Image</p>
-            @endif
         </div>
-            <div>
-                <span class="invalid-feedback" role="alert"></span>
+        <div class="form-group mt-4">
+            <label for="exampleInputPassword1">Link</label>
+            <input type="text" name="link" id="link" autocomplete="link" placeholder="http://www." class="form-control @error('link')
+                    is-invalid
+                @enderror" value="{{$slider->link }}" required aria-describedby="nameHelp">
+            @error('link') <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                @enderror
+        </div>
+        <div class="row align-items-start">
+            <div class="">
+                <label class="form-check-label mt-2" for="is_active">Is Active: {{ $slider->is_active === 1 ? 'yes' :
+                    'no'
+                    }}</label>
+                <div class="form-check form-switch">
+                    <input class="form-check-input " type="checkbox" role="switch" name="is_active" id="is_active" {{
+                        $slider->is_active === 1 ? 'checked' : '' }}>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary show_confirm_edit">Update</button>
+            <div class="col">
+                <div class="form-group">
+                    <label for="order">Order:</label>
+                    <input type="number" class="form-control" id="order" name="order" placeholder="order number" min="0"
+                        max="10" @error('order') is-invalid @enderror value="{{$slider->is_active}}"
+                        aria-describedby="nameHelp">
+                    @error('order') <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        @enderror
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <!-----image----->
+                    <label for="exampleInputPassword1">Change Image</label>
+                    <!---- filenames[]----->
+                    <input type="file" name="image" id="image" class="form-control @error('name')
+                            is-invalid
+                        @enderror" value="{{ $slider->image }}" aria-describedby="nameHelp">
+                    @error('image') <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                        @enderror
+                </div>
+                <!----------->
+            </div>
+            <div class="col">
+                <div class="w-50">
+                    <!----->
+                    @if ($slider->image)
+                    <img class="w-50 img-thumbnail" src="{{ asset('storage/' . $slider->image) }}" alt="image"></td>
+                    @else
+                    <p>No Image</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary show_confirm_edit mt-2">Update</button>
     </form>
 </div>
