7f07b1b89d7332e0e5490f8137c39e1efb152915
yeeysonduarte <portas3@hotmail.com>
2024-02-12 17:19:19 +0000

    fixes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,37 +12,36 @@ class PagesController extends Controller
 
     public function index() // Home
     {
-        $home = Page::where('title','Home')->where('is_active','1')->first();
-        $sliders = Slider::where('is_active','1')->get();
-      return view('index', ['home' => $home, 'sliders' => $sliders]);
+        $home = Page::where('title', 'Home')->where('is_active', '1')->first();
+        $sliders = Slider::where('is_active', '1')->get();
+        return view('index', ['home' => $home, 'sliders' => $sliders]);
     }
 
-    public function slider(){
-        $sliders = Slider::where('is_active','1')->get();
+    public function slider()
+    {
+        $sliders = Slider::where('is_active', '1')->get();
         return view('index', ['sliders' => $sliders]); // Homepage with Carousel
     }
 
 
     public function About()
     {
-        $about = Page::where('title','About')->where('is_active','1')->first();
-       //$page = Page::where('title','About')->first();
-      return view('pages/about', ['about' => $about]);
+        $about = Page::where('title', 'About')->where('is_active', '1')->first();
+        return view('pages/about', ['about' => $about]);
     }
 
     public function products()
     {
 
-        $productsPage = Page::where('title','Products')->where('is_active','1')->first();
+        $productsPage = Page::where('title', 'Products')->where('is_active', '1')->first();
         // $page = Page::where('title', 'Product')->first();
         // $products = Products::all();
-        return view('pages/products', ['productsPage'=> $productsPage ]);
+        return view('pages/products', ['productsPage' => $productsPage]);
     }
 
     public function Contact()
     {
-        $contact = Page::where('title','Contact')->where('is_active','1')->first();
-       //$page = Page::where('title','Contact')->first();
-      return view('pages/contact', ['contact' => $contact]);
+        $contact = Page::where('title', 'Contact')->where('is_active', '1')->first();
+        return view('pages/contact', ['contact' => $contact]);
     }
 }
