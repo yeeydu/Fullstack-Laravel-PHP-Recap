2c96456b58d370637d4af107c92b7f0f53397afd
yeeysonduarte <portas3@hotmail.com>
2024-02-15 19:21:07 +0000

    add product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Product_image;
+use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -17,8 +21,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = 'products';
-        return view('admin/pages/products', ['products', $products]);
+        $products = Product::orderBy('id', 'desc')->paginate(10);
+        $prod_images = Product_image::all();
+        return view('admin.pages.products.index', ['products' => $products, 'prod_images' => $prod_images]);
     }
 
     /**
@@ -26,7 +31,9 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        $subcategories = Subcategory::all();
+        return view('admin.pages.products.create', ['subcategories' => $subcategories, 'categories' => $categories]);
     }
 
     /**
@@ -34,7 +41,45 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'summary' => 'required|string',
+            'description' => 'required|string',
+            'brand' => 'required|string',
+            'color' => 'required|string',
+            'size' => 'required|string',
+            'price' => 'required|numeric',
+            'category_id' => 'required',
+            'subcategory_id' => 'required',
+            // 'is_active' => 'boolean',
+            'images.*'  => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust as needed
+        ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->summary = $request->summary;
+        $product->description = $request->description;
+        $product->brand = $request->brand;
+        $product->color = $request->color;
+        $product->size = $request->size;
+        $product->price = $request->price;
+        $product->category_id = $request->category_id;
+        $product->subcategory_id = $request->subcategory_id;
+        $product->is_active = $request->has('is_active') ? true : false;
+        $product->save(); // save it first to get the id and then pass it to the image
+
+        foreach ($request->file('images') as $image) {
+
+            $productImage = new Product_image();
+            $imageName = $image->getClientOriginalName();
+            $path = $image->storeAs('/images/products/' . $product->id, $imageName, 'public');
+            $productImage->url = $path;
+            $productImage->product_id = $product->id;
+            $productImage->save();
+        }
+        $product->save();
+
+        return redirect('admin/products')->with('msg', 'item created successfully.');
     }
 
     /**
