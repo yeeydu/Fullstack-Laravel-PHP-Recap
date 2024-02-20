7e88e51ede7663c8b29eb7a921af02f848c985c8
yeeysonduarte <portas3@hotmail.com>
2024-02-20 14:38:30 +0000

    add product inf link to card

--- resources/views/pages/productCard.blade.php
+++ resources/views/pages/productCard.blade.php
@@ -22,11 +22,14 @@
 <div class="card" style="width: 18rem; hover">
     @foreach ($images as $image)
     @if($image->product_id == $item->id)
-    <img class="card-img-top hover" src="{{ asset('storage/' . $image->url) }}" alt="image" style="object-fit: cover; width: 100%; height: 300px;"/>
+    <a href="{{url('product/' . $item->id . '/info')}}">
+        <img class="card-img-top hover" src="{{ asset('storage/' . $image->url) }}" alt="image"
+            style="object-fit: cover; width: 100%; height: 300px;" />
+    </a>
     @break {{-- Exit the loop after the first image --}}
     @endif
     @endforeach
-    <div class="card-body"  >
+    <div class="card-body">
         <h5 class="card-title">{{$item->name}}</h5>
         <p class="card-text">{{ number_format( $item->price, 2)}}€</p>
         <p class="card-text">
@@ -34,7 +37,8 @@
             <strong>Color:</strong> {{ $item->color}}<br>
             <strong>Size:</strong> {{ $item->size}}
         </p>
-        <p class="card-text">{{ strlen($item->summary) > 60 ? substr($item->summary, 0, 60) . ' ...' : $item->summary }}</p>
-        <a href="#" class="btn btn-outline-dark">More</a>
+        <p class="card-text">{{ strlen($item->summary) > 60 ? substr($item->summary, 0, 60) . ' ...' : $item->summary }}
+        </p>
+        <a href="{{url('product/' . $item->id . '/info')}}" class="btn btn-outline-dark">More</a>
     </div>
 </div>
