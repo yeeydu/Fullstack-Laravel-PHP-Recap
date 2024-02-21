9984680321ea94a8b4f70b02659835d1f4edd7b7
yeeysonduarte <portas3@hotmail.com>
2024-02-21 13:13:35 +0000

    add admin role to users table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Page;
 use App\Models\Products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -25,9 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //admin dashboard
+
+
+         if ( Auth::check() && Auth::user()->isAdmin() ) // admin ==1
+        {
+            //admin dashboard
          return view('admin.dashboard')->name('dashboard');
+        }else{
+
+            return redirect('/');
+        }
+
     }
 
 }
- 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class, // add admin permissions for back end access to users with is_admin=1
     ];
 }
