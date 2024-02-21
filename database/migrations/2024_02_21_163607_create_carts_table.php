4f4d0c36663014d40658ee5b5d71a3242f618107
yeeysonduarte <portas3@hotmail.com>
2024-02-21 17:11:20 +0000

    fixed migration

--- database/migrations/2024_02_21_163607_create_carts_table.php
+++ database/migrations/2024_02_21_163607_create_carts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->integer('product_id');
             $table->integer('user_id');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart');
+        Schema::dropIfExists('carts');
     }
 };
