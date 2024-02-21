eb506273e3f4127350a125e540d41e762eb2bab6
yeeysonduarte <portas3@hotmail.com>
2024-02-21 17:12:31 +0000

    add cart controller and model

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,21 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
     public function addToCart(Request $request)
     {
-        if ($request->session()->has('user')) {
 
-            $cart = new Cart();
-            $cart->user_id = $request->session()->get('user')['id'];
-            $cart->product_id = $request->product_id;
-            $cart->save();
-            return redirect('/products');
+        if (Auth::check()) {
 
+            $userId = Auth::id();
+            $productId = $request->input('product_id');
+
+            try {
+                $cart = new Cart();
+                $cart->user_id = $userId;
+                $cart->product_id = $productId;
+                $cart->save();
+                return redirect()->back()->with('msg', 'Item added to cart!');
+            } catch (\Exception $e) {
+                dd($e->getMessage());
+            }
         } else {
             return redirect('/login');
         }
+
     }
+
+
+    static function cartItem()
+    {
+        $userId = Auth::id();
+        return Cart::where('user_id', $userId)->count();
+    }
+
+
+    function cartList()
+    {
+
+    }
+
+
 }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
-    public $table='cart';
+    protected $fillable =
+    [
+        'user_id',
+        'product_id',
+    ];
 }
