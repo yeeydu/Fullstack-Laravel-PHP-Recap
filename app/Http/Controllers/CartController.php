fa8fb18d79584a6d4057024be2474513d44069a1
yeeysonduarte <portas3@hotmail.com>
2024-02-21 11:43:06 +0000

    add cart controller and model

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function addToCart(Request $request)
+    {
+        if ($request->session()->has('user')) {
+
+            $cart = new Cart();
+            $cart->user_id = $request->session()->get('user')['id'];
+            $cart->product_id = $request->product_id;
+            $cart->save();
+            return redirect('/products');
+
+        } else {
+            return redirect('/login');
+        }
+    }
+}
